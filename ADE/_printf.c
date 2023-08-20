92aa98a25d6d5ece847e0f709f513d5eb37dba21
Ademitidayo <adenijibimmy08@gmail.com>
2023-08-20 15:04:44 -0700

    test

---
+++ ADE/_printf.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _printf - function printing characters by handling c,s & %
+ * @format: string to print
+ * Return: -1 if void and 0 (success)
+ */
+void _printf(const char *format, ...)
+{
+	 va_list myzero;
+
+        va_start(myzero, format);
+
+        if(format == NULL)
+                return (-1);
+        while (*format)
+        {
+		format++;
+	}
+	cs_print(const char *format, ...);
+	percent_print(const char *format, ...);
+}
--- main.h
+++ main.h
@@ -1,11 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdarg.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdarg.h>
+#include <stdio.h>
 
 int _printf(const char *format, ...);
-int cs_print(const char *format, ...);
-int percent_print(const char *format, ...);
+int percent_printf(const char *format, ...);
+
 #endif
+
