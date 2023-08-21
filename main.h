183e4329835dc4819badf65b723881593a8724c2
Ademitidayo <adenijibimmy08@gmail.com>
2023-08-20 18:38:20 -0700

    printf

--- main.h
+++ main.h
@@ -8,5 +8,7 @@
 int _printf(const char *format, ...);
 int S_L(char *S);
 void _print_SP(char form, va_list mySP, int *zero_printed);
-
+int percent_print(const char *format, ...);
+int s_print(const char *format, ...);
+int c_print(const char *format, ...);
 #endif
