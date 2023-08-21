d1c5250ca64cd907769b3a46d7035e25104fc14a
Ogechukwu11 <nwobumogechukwu@gmail.com>
2023-08-21 08:34:59 -0700

    please move

--- main.h
+++ main.h
@@ -6,10 +6,9 @@
 #include <unistd.h>
 
 int _printf(const char *format, ...);
-int S_L(char *S);
-void _print_SP(char form, va_list mySP, int *zero_printed);
 int percent_print(va_list myzero);
 int s_print(va_list myzero);
 int c_print(va_list myzero);
 int all_print(const char format, va_list myzero);
+
 #endif
