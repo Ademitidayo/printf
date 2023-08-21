190178a1308a70e9814743dc4b5216ca42e3e43f
Ogechukwu11 <nwobumogechukwu@gmail.com>
2023-08-21 07:41:12 -0700

    it is over

---
+++ printf/_printf.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * _printf - function printing characters by handling c,s & %
+ * @format: string to print
+ * Return: -1 if void and 0 (success)
+ */
+int _printf(const char *format, ...)
+{
+	int zero_printed = 0;
+	va_list myzero;
+
+	va_start(myzero, format);
+
+	if (format == NULL)
+		return (-1);
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			zero_printed++;
+		}
+		else
+		{
+			format++;
+		if (*format != '\0')
+		{
+			zero_printed += all_print(*format, myzero);
+		}
+		else
+			break;
+		}
+		format++;
+	}
+	va_end(myzero);
+	return (zero_printed);
+}
