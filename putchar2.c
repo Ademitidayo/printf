8609ef7777aed3966a3f8a9c1c3383017422ef67
Ogechukwu11 <nwobumogechukwu@gmail.com>
2023-08-22 08:49:26 -0700

    changes

--- putchar2.c
+++ putchar2.c
@@ -1,12 +1,12 @@
 #include <unistd.h>
 
 /**
- * _putchar - a function to write the characters to the stdouput
+ * _putchar2 - a function to write the characters to the stdouput
  * @ch: The printed character
  *
  * Return: On success 1 and -1 on error
  */
-int _putchar(char ch)
+int _putchar2(char ch)
 {
 	if (ch != -1)
 	{
