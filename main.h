0952468c7506083ea7cf5c542e5d4493f65bacde
Ademitidayo <adenijibimmy08@gmail.com>
2023-08-21 21:22:23 -0700

    debugging

--- main.h
+++ main.h
@@ -4,7 +4,6 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <stdio.h>
 int _printf(const char *format, ...);
 int percent_print(va_list myzero);
 int s_print(va_list myzero);
