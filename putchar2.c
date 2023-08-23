76dcfbea2c22201bcf67a0fa2df41ce5fb024303
Ogechukwu11 <nwobumogechukwu@gmail.com>
2023-08-22 17:55:16 -0700

    write function

--- putchar2.c
+++ putchar2.c
@@ -8,9 +8,17 @@
  */
 int _putchar2(char ch)
 {
+	static char bu[1024];
+	static count = 0;
+
+	if (ch == -1 || count >= 1024)
+	{
+		write(1, &bu, 1);
+	}
 	if (ch != -1)
 	{
-		write(1, &ch, 1);
+		bu[count] = ch;
+		count++;
 	}
 	return (1);
 }
